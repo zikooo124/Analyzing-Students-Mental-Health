0dd60174de7998aed355706dc65c6d1a46a04720
Ziad-Hesham <zhesham57@gmail.com>
2026-03-06 03:47:24 +0200

    Create query.sql

---
+++ query.sql
@@ -0,0 +1,10 @@
+SELECT stay,
+       COUNT(*) AS count_int,
+	 ROUND(AVG(todep),2) AS average_phq,
+	 ROUND(AVG(tosc),2)	 AS average_scs,
+	 ROUND(AVG(toas),2)  AS average_as
+FROM students
+WHERE inter_dom='Inter'	
+GROUP BY stay
+ORDER BY stay DESC
+LIMIT 9;
